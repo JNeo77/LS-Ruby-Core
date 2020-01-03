62b994642e6ca97212e1fea245903167335b01df
Jim Oursler <lifted_2000@yahoo.com>
2020-01-03 09:53:41 -0500

    Loan Payment Calculator Refactored

--- RB101_Foundations/small_programs/payment_calculator.rb
+++ RB101_Foundations/small_programs/payment_calculator.rb
@@ -13,7 +13,7 @@ def float?(input)
 end
 
 def valid_number?(input)
-  integer?(input) || float?(input)
+  (integer?(input) || float?(input)) && input.to_i > 0
 end
 
 def apr_to_decimal(apr)
@@ -32,44 +32,61 @@ def payment(loan_amount, rate, duration)
   loan_amount * (rate / (1 - (1 + rate)**(-duration)))
 end
 
-prompt("Welcome to the Loan Payment Calculator!")
+def retrieve_play_again_answer
+  prompt("Would you like to perform another calculation?(Y to calculate again or N to exit)")
+  answer = Kernel.gets().chomp()
+end
 
-loop do # main loop
-  loan_amount = ''
+def new_calculation?(answer)
+  answer == "y" || answer == "yes"
+end
+
+def retrieve_loan_amount
   loop do
     prompt("Enter the loan amount:")
     loan_amount = Kernel.gets().chomp()
-
+    
     if valid_number?(loan_amount)
-      break
+      return loan_amount
     else
       prompt("Hmm... that doesn't look like a valid loan amount.")
     end
   end
+end
 
-  apr = ''
+def retrieve_apr
   loop do
     prompt("Enter the Annual Percentage Rate:")
     apr = Kernel.gets().chomp()
 
     if valid_number?(apr)
-      break
+      return apr
     else
       prompt("Hmm... that doesn't look like a valid APR.")
     end
   end
+end
 
-  duration_in_years = ''
+def retrieve_duration_in_years
   loop do
     prompt("Enter the loan duration (in years):")
     duration_in_years = Kernel.gets().chomp()
 
     if valid_number?(duration_in_years)
-      break
+      return duration_in_years
     else
       prompt("Hmm... that doesn't look like a valid duration.")
     end
   end
+end
+
+prompt("Welcome to the Loan Payment Calculator!")
+
+loop do # main loop
+  loan_amount = retrieve_loan_amount
+  apr = retrieve_apr
+  duration_in_years = retrieve_duration_in_years
+  
 
   apr_decimal = apr_to_decimal(apr.to_f)
   monthly_rate = apr_to_month(apr_decimal)
@@ -78,9 +95,8 @@ loop do # main loop
   monthly_payment = payment(loan_amount.to_i, monthly_rate, duration_in_months)
 
   prompt("Your monthly payment is $#{monthly_payment.round(2)}!")
-  prompt("Would you like to perform another calculation?(Y to calculate again)")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  answer = retrieve_play_again_answer
+  break unless new_calculation?(answer)
 end
 
 prompt("Thank you for using the Loan Payment Calculator. Good bye!")
