9111854318aa0c7494a14f7f67139db6aeffad11
Jim Oursler <lifted_2000@yahoo.com>
2021-05-23 21:38:23 +0000

    Redoing Collections

--- RB101_Foundations/ruby_collections/loops1/say_hello.rb
+++
@@ -1,8 +0,0 @@
-say_hello = true
-iterator = 0
-
-while say_hello
-  puts 'Hello!'
-	iterator += 1
-	say_hello = false if iterator == 5
-end
--- RB101_Foundations/ruby_collections/numbers.rb
+++
@@ -1,9 +0,0 @@
-numbers = [7, 3, 5, 2, 1, 8, 4]
-
-numbers.each do |number|
-  counter = 0
-  number.times do
-    counter += 1
-    puts counter
-  end
-end
